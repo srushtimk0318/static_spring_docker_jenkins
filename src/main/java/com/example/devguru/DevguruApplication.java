05affc253f4ff0323716eef46ac71e91cbdbab6c
srushtimk0318 <sry@gmail.com>
2025-11-20 21:35:03 +0530

    file executed

---
+++ src/main/java/com/example/devguru/DevguruApplication.java
@@ -0,0 +1,13 @@
+package com.example.devguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devguru/DevguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
